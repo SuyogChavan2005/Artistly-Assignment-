dd7e6abe76172c9271bdf423794c84386275abdb
Suyog Chavan <chavansuyog2005@gmail.com>
2025-06-25 16:43:27 +0530

    Add files via upload

---
+++ src/assets/assets.js
@@ -0,0 +1,299 @@
+import appointment_img from './appointment_img.png'
+import header_img from './header_img.png'
+import group_profiles from './group_profiles.png'
+import profile_pic from './profile_pic.png'
+import contact_image from './contact_image.jpg'
+import about_image from './about_image.jpg'
+import logo from './logo.svg'
+import dropdown_icon from './dropdown_icon.svg'
+import menu_icon from './menu_icon.svg'
+import cross_icon from './cross_icon.png'
+import chats_icon from './chats_icon.svg'
+import verified_icon from './verified_icon.svg'
+import arrow_icon from './arrow_icon.svg'
+import info_icon from './info_icon.svg'
+import upload_icon from './upload_icon.png'
+import stripe_logo from './stripe_logo.png'
+import razorpay_logo from './razorpay_logo.png'
+import doc1 from './doc1.png'
+import doc2 from './doc2.png'
+import doc3 from './doc3.png'
+import doc4 from './doc4.png'
+import doc5 from './doc5.png'
+import doc6 from './doc6.png'
+import doc7 from './doc7.png'
+import doc8 from './doc8.png'
+import doc9 from './doc9.png'
+import doc10 from './doc10.png'
+import doc11 from './doc11.png'
+import doc12 from './doc12.png'
+import doc13 from './doc13.png'
+import doc14 from './doc14.png'
+import doc15 from './doc15.png'
+import Painting from './Painting.svg'
+import Music from './Music.svg'
+import Sculpture from './Sculpture.svg'
+import Architecture from './Architecture.svg'
+import Literature from './Literature.svg'
+import Theater from './Theater.svg'
+
+
+export const assets = {
+    appointment_img,
+    header_img,
+    group_profiles,
+    logo,
+    chats_icon,
+    verified_icon,
+    info_icon,
+    profile_pic,
+    arrow_icon,
+    contact_image,
+    about_image,
+    menu_icon,
+    cross_icon,
+    dropdown_icon,
+    upload_icon,
+    stripe_logo,
+    razorpay_logo
+}
+
+export const specialityData = [
+    {
+        speciality: 'Painting',
+        image: Painting
+    },
+    {
+        speciality: 'Music',
+        image: Music
+    },
+    {
+        speciality: 'Sculpture',
+        image: Sculpture
+    },
+    {
+        speciality: 'Architecture',
+        image: Architecture
+    },
+    {
+        speciality: 'Literature',
+        image: Literature
+    },
+    {
+        speciality: 'Theater',
+        image: Theater
+    },
+]
+
+export const doctors = [
+    {
+        _id: 'doc1',
+        name: 'Richard James',
+        image: doc1,
+        speciality: 'Painting',
+        degree: 'MBBS',
+        experience: '4 Years',
+        about: 'Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies. Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies.',
+        fees: 50000,
+        address: {
+            line1: '17th Cross, Richmond',
+            line2: 'Circle, Ring Road, London'
+        }
+    },
+    {
+        _id: 'doc2',
+        name: 'Emily Larson',
+        image: doc2,
+        speciality: 'Music',
+        degree: 'MBBS',
+        experience: '3 Years',
+        about: 'Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies. Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies.',
+        fees: 60000,
+        address: {
+            line1: '27th Cross, Richmond',
+            line2: 'Circle, Ring Road, London'
+        }
+    },
+    {
+        _id: 'doc3',
+        name: 'Sarah Patel',
+        image: doc3,
+        speciality: 'Sculpture',
+        degree: 'MBBS',
+        experience: '1 Years',
+        about: 'Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies. Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies.',
+        fees: 30000,
+        address: {
+            line1: '37th Cross, Richmond',
+            line2: 'Circle, Ring Road, London'
+        }
+    },
+    {
+        _id: 'doc4',
+        name: 'Christopher Lee',
+        image: doc4,
+        speciality: 'Architecture',
+        degree: 'MBBS',
+        experience: '2 Years',
+        about: 'Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies. Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies.',
+        fees: 40000,
+        address: {
+            line1: '47th Cross, Richmond',
+            line2: 'Circle, Ring Road, London'
+        }
+    },
+    {
+        _id: 'doc5',
+        name: 'Jennifer Garcia',
+        image: doc5,
+        speciality: 'Literature',
+        degree: 'MBBS',
+        experience: '4 Years',
+        about: 'Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies. Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies.',
+        fees: 50000,
+        address: {
+            line1: '57th Cross, Richmond',
+            line2: 'Circle, Ring Road, London'
+        }
+    },
+    {
+        _id: 'doc6',
+        name: 'Andrew Williams',
+        image: doc6,
+        speciality: 'Theater',
+        degree: 'MBBS',
+        experience: '4 Years',
+        about: 'Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies. Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies.',
+        fees: 50000,
+        address: {
+            line1: '57th Cross, Richmond',
+            line2: 'Circle, Ring Road, London'
+        }
+    },
+    {
+        _id: 'doc7',
+        name: 'Christopher Davis',
+        image: doc7,
+        speciality: 'Theater',
+        degree: 'MBBS',
+        experience: '4 Years',
+        about: 'Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies. Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies.',
+        fees: 50000,
+        address: {
+            line1: '17th Cross, Richmond',
+            line2: 'Circle, Ring Road, London'
+        }
+    },
+    {
+        _id: 'doc8',
+        name: 'Timothy White',
+        image: doc8,
+        speciality: 'Music',
+        degree: 'MBBS',
+        experience: '3 Years',
+        about: 'Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies. Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies.',
+        fees: 60000,
+        address: {
+            line1: '27th Cross, Richmond',
+            line2: 'Circle, Ring Road, London'
+        }
+    },
+    {
+        _id: 'doc9',
+        name: 'Ava Mitchell',
+        image: doc9,
+        speciality: 'Painting',
+        degree: 'MBBS',
+        experience: '1 Years',
+        about: 'Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies. Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies.',
+        fees: 30000,
+        address: {
+            line1: '37th Cross, Richmond',
+            line2: 'Circle, Ring Road, London'
+        }
+    },
+    {
+        _id: 'doc10',
+        name: 'Jeffrey King',
+        image: doc10,
+        speciality: 'Theater',
+        degree: 'MBBS',
+        experience: '2 Years',
+        about: 'Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies. Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies.',
+        fees: 40000,
+        address: {
+            line1: '47th Cross, Richmond',
+            line2: 'Circle, Ring Road, London'
+        }
+    },
+    {
+        _id: 'doc11',
+        name: 'Zoe Kelly',
+        image: doc11,
+        speciality: 'Painting',
+        degree: 'MBBS',
+        experience: '4 Years',
+        about: 'Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies. Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies.',
+        fees: 50000,
+        address: {
+            line1: '57th Cross, Richmond',
+            line2: 'Circle, Ring Road, London'
+        }
+    },
+    {
+        _id: 'doc12',
+        name: 'Patrick Harris',
+        image: doc12,
+        speciality: 'Literature',
+        degree: 'MBBS',
+        experience: '4 Years',
+        about: 'Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies. Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies.',
+        fees: 50000,
+        address: {
+            line1: '57th Cross, Richmond',
+            line2: 'Circle, Ring Road, London'
+        }
+    },
+    {
+        _id: 'doc13',
+        name: 'Chloe Evans',
+        image: doc13,
+        speciality: 'Music',
+        degree: 'MBBS',
+        experience: '4 Years',
+        about: 'Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies. Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies.',
+        fees: 50000,
+        address: {
+            line1: '17th Cross, Richmond',
+            line2: 'Circle, Ring Road, London'
+        }
+    },
+    {
+        _id: 'doc14',
+        name: 'Ryan Martinez',
+        image: doc14,
+        speciality: 'Sculpture',
+        degree: 'MBBS',
+        experience: '3 Years',
+        about: 'Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies. Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies.',
+        fees: 60000,
+        address: {
+            line1: '27th Cross, Richmond',
+            line2: 'Circle, Ring Road, London'
+        }
+    },
+    {
+        _id: 'doc15',
+        name: 'Amelia Hill',
+        image: doc15,
+        speciality: 'Theater',
+        degree: 'MBBS',
+        experience: '1 Years',
+        about: 'Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies. Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies.',
+        fees: 30000,
+        address: {
+            line1: '37th Cross, Richmond',
+            line2: 'Circle, Ring Road, London'
+        }
+    },
+]
---
+++ src/pages/Doctors.jsx
@@ -0,0 +1,60 @@
+import React, { useContext, useEffect, useState } from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
+import { AppContext } from '../context/AppContext'
+
+const Doctors = () => {
+
+  const { speciality } = useParams()
+  const [filterDoc, setFilterDoc] = useState([])
+  const [showFilter, setShowFilter] = useState(false)
+  const navigate = useNavigate() 
+
+  const { doctors } = useContext(AppContext)
+
+  const applyFilter = () => {
+    if (speciality) {
+      setFilterDoc(doctors.filter(doc => doc.speciality === speciality)) 
+    } else {
+      setFilterDoc(doctors) // ✅ fixed syntax
+    }
+  }
+
+  useEffect(() => {
+    applyFilter()
+  }, [doctors, speciality])
+
+  return (
+    <div>
+      <p className='text-gray-600'>Browse through the artists specialist.</p>
+      <div className='flex flex-col sm:flex-row items-start gap-5 mt-5'>
+        <button className={`py-1 px-3 border rounded text-sm transition-all sm:hidden ${showFilter ? 'bg-primary text-white' : ''}`} onClick={()=>setShowFilter(prev => !prev)}>Filters</button>
+        <div className={`flex-col gap-4 text-sm text-gray-600 ${ showFilter ? 'flex' : 'hidden sm:flex'}`}>
+          <p onClick={()=> speciality === 'Painting' ? navigate('/doctors') : navigate('/doctors/Painting')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === "Painting" ? "bg-indigo-100 text-black":"" }`}>Painting</p>
+          <p onClick={()=> speciality === 'Music' ? navigate('/doctors') : navigate('/doctors/Music')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === "Music" ? "bg-indigo-100 text-black":"" }`}>Music</p>
+          <p onClick={()=> speciality === 'Sculpture' ? navigate('/doctors') : navigate('/doctors/Sculpture')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === "Sculpture" ? "bg-indigo-100 text-black":"" }`}>Sculpture</p>
+          <p onClick={()=> speciality === 'Architecture' ? navigate('/doctors') : navigate('/doctors/Architecture')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === "Architecture" ? "bg-indigo-100 text-black":"" }`}>Architecture</p>
+          <p onClick={()=> speciality === 'Literature' ? navigate('/doctors') : navigate('/doctors/Literature')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === "Literature" ? "bg-indigo-100 text-black":"" }`}>Literature</p>
+          <p onClick={()=> speciality === 'Theater' ? navigate('/doctors') : navigate('/doctors/Theater')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === "Theater" ? "bg-indigo-100 text-black":"" }`}>Theater</p>
+        </div>
+        <div className='w-full grid grid-cols-auto gap-4 gap-y-6'>
+          {
+            filterDoc.map((item, index) => (
+              <div onClick={() => navigate(`/appointment/${item._id}`)} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index}>
+                <img className='bg-blue-50' src={item.image} alt="" />
+                <div className='p-4'>
+                  <div className='flex items-center gap-2 text-sm text-center text-green-500'>
+                    <p className='w-2 h-2 bg-green-500 rounded-full'></p><p>Available</p>
+                  </div>
+                  <p className='text-gray-900 text-lg font-medium'>{item.name}</p>
+                  <p className='text-gray-600 text-sm'>{item.speciality}</p>
+                </div>
+              </div>
+            ))
+          }
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Doctors
